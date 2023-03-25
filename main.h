e079a57413f892a9c34853ff0d03d272b59de0e9
WatipasoChirambo <watipasochirambo5@gmail.com>
2023-03-25 14:09:37 -0700

    Main H modified

---
+++ main.h
@@ -0,0 +1,29 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * struct convert - defines a structure for symbols and functions
+ *
+ * @sym: The operator
+ * @f: The function associated with a specifier
+ */
+
+struct convert
+{
+	char *sym;
+	int (*f)(va_list);
+};
+typedef struct convert conver_t;
+
+/* Main Features */
+int parser(const char *format, conver_t f_list[], va_list arg_list);
+int _printf(const char *format, ...);
+int print_char(va_list);
+int print_string(va_list);
+int print_percent(va_list);
+
+#endif
