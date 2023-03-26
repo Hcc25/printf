f5ea1697469bc7d81c58ee88ac2d8eee19c29aac
Hcc25 <hildacheptekei@outlook.com>
2023-03-25 17:48:02 -0700

    _print.c

--- _printf.c
+++ _printf.c
@@ -15,22 +15,14 @@ int _printf(const char *format, ...)
 		{"%", print_percent},
 		{"d", print_integer},
 		{"i", print_integer},
-		{"b", print_binary},
-		{"r", print_reversed},
-		{"R", rot13},
-		{"u", unsigned_integer},
-		{"o", print_octal},
-		{"x", print_hex},
-		{"X", print_heX},
 		{NULL, NULL}
 	};
 	va_list arg_list;
 
-	if (format == NULL)
+	if (formart == NULL)
 		return (-1);
-
-	va_start(arg_list, format);
-	/*Calling parser function*/
+	va_start(arg_list, formart);
+	/*calling parser Function*/
 	printed_chars = parser(format, f_list, arg_list);
 	va_end(arg_list);
 	return (printed_chars);
