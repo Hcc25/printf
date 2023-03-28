12e38805934509dfae2f2e0ff6ef48a1a0279ec4
WatipasoChirambo <watipasochirambo5@gmail.com>
2023-03-28 07:53:57 -0700

    Fix 001

--- main.h
+++ main.h
@@ -27,9 +27,14 @@ int print_string(va_list);
 int print_percent(va_list);
 int print_integer(va_list);
 int print_number(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list list);
+int print_hex(va_list list);
+int print_heX(va_list list);
 int print_binary(va_list);
 int print_reversed(va_list arg);
 int rot13(va_list);
+
 
 /* utility functions */
 unsigned int base_len(unsigned int, int);
