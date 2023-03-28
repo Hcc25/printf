a4506988a3f33d6c090d315f95a3944ac4a8cb9b
WatipasoChirambo <watipasochirambo5@gmail.com>
2023-03-28 04:49:28 -0700

    Task 2-n

--- _printf.c
+++ _printf.c
@@ -16,6 +16,8 @@ int _printf(const char *format, ...)
 		{"i", print_integer},
 		{"%", print_percent},
 		{"b", print_binary},
+		{"r", print_reversed},
+		{"R", rot13},
 		{"u", unsigned_integer},
 		{"o", print_octal},
 		{"x", print_hex},
